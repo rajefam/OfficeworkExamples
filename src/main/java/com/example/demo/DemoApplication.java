c37dc5b9ba08b0b40e6c2a1529ca4144d826dbc7
rajefam <rajeswarireddy514@gmail.com>
2022-10-31 12:53:54 +0530

    changes added in demo

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 	System.out.println("DemoApplication.main()");
+	System.out.println("welcome to develop");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
